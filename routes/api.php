d97bf99a9908c4bf5e7322bc6e41a2fd3b50855c
plektrum24 <mhaikal20@gmail.com>
2026-01-24 17:42:28 +0700

    Tahap Awal Migrasi Backend Selesai

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 
 // Public Routes
 Route::post('/login', [AuthController::class, 'login']);
@@ -14,4 +15,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    // Master Data & Inventory
+    Route::apiResource('products', ProductController::class);
 });
